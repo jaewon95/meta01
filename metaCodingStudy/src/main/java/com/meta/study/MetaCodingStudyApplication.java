aecb977cd96d86f2b98c2c370a577369a54db772
jaewon <winangil153@gmail.com>
2023-03-14 12:41:05 +0900

    강의 시작

---
+++ metaCodingStudy/src/main/java/com/meta/study/MetaCodingStudyApplication.java
@@ -0,0 +1,13 @@
+package com.meta.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaCodingStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaCodingStudyApplication.class, args);
+	}
+
+}
---
+++ metaCodingStudy/src/test/java/com/meta/study/MetaCodingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaCodingStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
